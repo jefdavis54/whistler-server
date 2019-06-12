d1c3ab2aa98fe2ab738e9984630b4f0bb8be18d5
Jeff <jdavisnj@gmail.com>
2019-06-12 13:35:46 -0400

    Fixed type error typescript caught.

--- src/util/responseShaperPRISMA.ts
+++ src/util/responseShaperPRISMA.ts
@@ -1,16 +1,20 @@
 import { errObj } from "./responseShaperSERVER";
 import { ERR_03_NO_DELETEDAT_FLAG } from "../lib/errorMessages";
-import { PrismaResponse } from '../lib/typsescriptInterfaces'
+import { PrismaResponse } from "../lib/typsescriptInterfaces";
 // zJED: My convention is to have this provide a consistent return object to all prisma Query & Mutation requests.
 // zJED: This should make it easier, for example, to remove records from all mutation prisma responses with a deletedAt flag set to something other than null
 // zJED: May also help if I want to append a caller to the errors returned in the future.
 // zJED: It also allows for more consistent processing and summarizing of Prisma Errors vs Server Errors
 
-type PrismaParam = any[]
+type PrismaParam = any[];
 
-async function prismaResponseShaper(prismaFn: any, prismaPar: PrismaParam = [], includeDeletedRecords = false) {
+async function prismaResponseShaper(
+  prismaFn: any,
+  prismaPar: PrismaParam = [],
+  includeDeletedRecords = false,
+) {
   // zJED TODO: Add a string length checker.
-  const returnObject: PrismaResponse = { errors: [], data: { deletedAt: null } };
+  const returnObject: PrismaResponse = { errors: [], data: { deletedAt: undefined } };
   try {
     const prismaResults = await prismaFn(...prismaPar);
     if (Array.isArray(prismaResults.errors)) {
@@ -44,7 +48,7 @@ async function prismaResponseShaper(prismaFn: any, prismaPar: PrismaParam = [], 
     // zJED TODO PROD: These messages may want to switch to NOT AUTHORIZED since they expose the inner working of the parent prisma database.
     // console.log("prismaResponseShaper:catch block::\n", err);
     // console.log("prismaResponseShaper:catch block:message::\n", err.message);
-    const msg = err.message ? err.message : JSON.stringify(err)
+    const msg = err.message ? err.message : JSON.stringify(err);
     returnObject.errors.push(`ERROR:PRISMA:responseShaperPrisma try-catch::${msg}`);
     return returnObject;
   }
